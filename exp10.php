0df91bbc5776fcfe1983ff93c02fc4a66ef4574c
prAtikp61 <pratikppawar2004@gmail.com>
2024-03-26 02:07:07 +0530

    done

--- exp10.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-$_SESSION['username'] = 'John';
-$_SESSION['email'] = 'john@example.com';
-?>
--- login.php
+++ login.php
@@ -1,29 +1,35 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $servername=$_POST['Localhost']; 
+    $servername = "localhost";
     $username = $_POST['Username'];
-    $password = $_POST['Password'];
-    $conn =mysqli_connect('3306', 'root', 'W7301@jqir#', 'patil');
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    $password = $_POST['password'];
+    $conn = mysqli_connect('localhost', 'root', '', 'project');
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
-    $query = "SELECT * FROM users WHERE Username='$username'";
+
+    $query = "SELECT * FROM users WHERE username='$username'";
     $result = $conn->query($query);
 
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
         if (password_verify($password, $row['password_hash'])) {
             $_SESSION['username'] = $username;
-            header("Location: dashboard.php");
-            exit(); 
+            header("Location: final.html");
+            exit();
         } else {
             echo "Invalid Username or Password.";
         }
     } else {
         echo "Invalid Username or Password.";
     }
+
     $conn->close();
 }
 ?>
